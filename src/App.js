cf4a6e1191b0ddaa38dff60e6f54aadeb094b8d8
mike mambwe <mikeapple056@gmail.com>
2023-04-01 20:09:26 +0200

    main component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import {Chatbot } from 'react-chatbot-kit'
+
+const App = () => {
+  return (
+    <div className="App">
+      K{30000}
+      <div className="bg">
+        <Chatbot />
+      </div>
+    </div>
+  );
+}
+
+export default App
